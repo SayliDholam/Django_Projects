6ce2c27c238798c3bba874459c6e681f39d5259a
Sayli R Dholam <saylidholam08@gmail.com>
2025-07-30 11:41:19 +0530

    updated project urls

--- myLearn/myLearn/urls.py
+++ myLearn/myLearn/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Hello_World.urls')),  
+    path('calc/', include('Calc.urls')),
 ]
