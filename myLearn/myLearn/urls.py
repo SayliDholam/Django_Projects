c2b7bb0acadbc9c96bf21d401e7d2e4cf56c75d1
Sayli R Dholam <saylidholam08@gmail.com>
2025-07-31 12:52:01 +0530

    updated project urls

--- myLearn/myLearn/urls.py
+++ myLearn/myLearn/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Hello_World.urls')),  
     path('calc/', include('Calc.urls')),
+    path('ToDo/', include('ToDo.urls')),
 ]
